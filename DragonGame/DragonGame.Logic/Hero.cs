5c652e79b47bb025f4c50115c7823394a91e8cb6
IxI-Enki <cell.enohp.apps@gmail.com>
2024-07-18 09:55:42 +0200

    reworked hero

--- DragonGame/DragonGame.Logic/Hero.cs
+++ DragonGame/DragonGame.Logic/Hero.cs
@@ -1,11 +1,37 @@
-﻿internal static class Hero
-{
-  public static Entity entity;
+﻿namespace DragonGame.Logic;
 
-  static Hero()
-  => entity = new()
+public class Hero : Sprite
+{
+  private const float SPEED = 500;
+  private Vector2 _minPos, _maxPos;
+
+  public Hero(Texture2D texture, Vector2 position) : base(texture, position)
   {
-    Position = new Vector2(20f, 20f),
-    Sprite = 'x'
-  };
+  }
+
+  public void SetBounds(Point mapSize, Point tileSize)
+  {
+    _minPos = new(
+      (-tileSize.X / 2) + Origin.X, (-tileSize.Y / 2) + Origin.Y);
+    _maxPos = new(
+      mapSize.X - (tileSize.X / 2) - Origin.X,
+      mapSize.Y - (tileSize.Y / 2) - Origin.Y);
+  }
+  public void Update()
+  {
+    Position += InputManager.Direction * Globals.Time * SPEED;
+    Position = Vector2.Clamp(Position, _minPos, _maxPos);
+  }
 }
+
+#region outdated
+/* public static Entity entity;
+
+ static Hero()
+ => entity = new()
+ {
+   Position = new Vector2(20f, 20f),
+   Sprite = 'x'
+ };
+*/
+#endregion outdated
